cf7e50af7e9e2c82aa3d87ad90483146b1afa135
bulbucus <emanuelfarinha98@gmail.com>
2021-02-04 21:59:49 +0000

    add favicon

--- app.js
+++ app.js
@@ -1,3 +1,6 @@
+const fs = require('fs');
+const http = require('http');
+const https = require('https');
 const express = require('express');
 const path = require('path');
 
@@ -6,6 +9,17 @@ require('dotenv').config()
 
 // router path
 const indexRouter = require('./routes/index');
+
+// Certificate
+const privateKey = fs.readFileSync('/etc/letsencrypt/live/sevegan.xyz/privkey.pem', 'utf8');
+const certificate = fs.readFileSync('/etc/letsencrypt/live/sevegan.xyz/cert.pem', 'utf8');
+const ca = fs.readFileSync('/etc/letsencrypt/live/sevegan.xyz/chain.pem', 'utf8');
+
+const credentials = {
+	key: privateKey,
+	cert: certificate,
+	ca: ca
+};
 
 const app = express();
 
@@ -26,6 +40,8 @@ app.use((req, res) => {
   res.send({message: 'Nothing to see were'})
 });
 
-app.listen(80, () => {
-  console.log('server is up')
-})
+const httpsServer = https.createServer(credentials, app);
+
+httpsServer.listen(443, () => {
+	console.log('HTTPS Server running on port 443');
+});
