e1439f2a040448c505780c7d3b11826ad7818d6c
bulbucus <emanuelfarinha98@gmail.com>
2021-02-04 19:02:32 +0000

    remove helmet and 443

--- app.js
+++ app.js
@@ -1,7 +1,5 @@
 const express = require('express');
 const path = require('path');
-const helmet = require('helmet')
-const logger = require('morgan');
 
 // for .env working
 require('dotenv').config()
@@ -11,11 +9,8 @@ const indexRouter = require('./routes/index');
 
 const app = express();
 
-app.use(helmet());
 // view engine setup
 app.set('view engine', 'ejs');
-// para ver os logs no command-line
-app.use(logger('dev'));
 // middleware para modificar os parametros recebidos para um objecto
 app.use(express.json());
 app.use(express.urlencoded({extended:false}))
@@ -32,9 +27,5 @@ app.use((req, res) => {
 });
 
 app.listen(80, () => {
-  console.log('server is up')
-})
-
-app.listen(443, () => {
   console.log('server is up')
 })
