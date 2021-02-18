2c50edbac29f0d6708d4e5f7f389e4c191a8c2a0
bulbucus <emanuelfarinha98@gmail.com>
2021-02-18 17:33:44 +0000

    change deploy.sh 2

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-ssh -i /c/Users/emanu/.ssh/seveganwebsite seveganwebsite@35.237.93.95  "cd server && git pull && sudo docker-compose down && sudo docker-compose up -d"
+ssh -i /c/Users/emanu/.ssh/seveganwebsite seveganwebsite@35.237.93.95  "cd sevegan && git pull && sudo docker-compose down && sudo docker-compose up -d"
